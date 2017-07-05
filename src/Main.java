d07df539c4b7a4c73b66b55403ea70c089e1f80a
chengjiao <chengjiaosky@gmail.com>
2017-07-05 15:31:09 +0800

    增加计分功能

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int count = 0;
         Point point = new Point((int)(Math.random()*9),(int)(Math.random()*9));
         Point snake = new Point((int)(Math.random()*9),(int)(Math.random()*9));
         Graphics g = new Graphics();
@@ -22,12 +23,18 @@ public class Main {
             g.draw(point,snake);
             if(snake.getX()<0 || snake.getX()>10){
                 System.out.println("You dead!X");
+                count = 0;
+                System.out.println(count);
                 break;
             }else if(snake.getY()<0 || snake.getY()>10){
                 System.out.println("You dead!Y");
+                count = 0;
+                System.out.println(count);
                 break;
             }else if (snake.getX()==point.getX()&&snake.getY()==point.getY()){
                 System.out.println("You win!");
+                count++;
+                System.out.println(count);
                 point = new Point((int)(Math.random()*9),(int)(Math.random()*9));
                 g.draw(point,snake);
             }else {
